a351cfd98bff2607d9d6304bf24697fc376511c5
Vladimir Kurchatkin <vladimir.kurchatkin@gmail.com>
2016-05-25 20:01:06 +0300

    App: meta

--- src/app/types.js
+++ src/app/types.js
@@ -1,1 +1,14 @@
 /* @flow */
+
+export type AppMeta = {
+  name: string,
+  version: string
+};
+
+
+export type App = {
+  meta?: AppMeta,
+  // standart health check for service registration
+  isHealthy?: () => Promise<boolean>,
+  getPendingTransactions?: () => number
+};
