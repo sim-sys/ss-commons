cd193f5728b57b1546bec719a2eccfdbcd36166c
Vladimir Kurchatkin <vladimir.kurchatkin@gmail.com>
2016-06-08 17:26:52 +0300

    fix eslint issue

--- src/service/util.js
+++ src/service/util.js
@@ -10,7 +10,7 @@ import type Context from './Context.js';
 export function applyFilter<ReqIn, RepOut, ReqOut, RepIn>(
   service: Service<ReqOut, RepIn>,
   filter: Filter<ReqIn, RepOut, ReqOut, RepIn>
-): Service<ReqIn, RepOut>  {
+): Service<ReqIn, RepOut> {
   return {
     call(req: ReqIn, ctx: ?Context): Promise<RepOut> {
       return filter.apply(req, ctx, service);
