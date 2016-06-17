0550a9f810724c44e772b0ac79cb1197f64c4b23
Vladimir Kurchatkin <vladimir.kurchatkin@gmail.com>
2016-06-17 19:11:39 +0300

    tmp

--- src/core/util.js
+++ src/core/util.js
@@ -6,6 +6,11 @@ import type {
   Result
 } from '../core/types.js';
 
+import {
+  andResult,
+  andResultFn
+} from './unsafe.js';
+
 // force-unwrap optional value
 export function unwrap<T>(val: ?T): T {
   if (!val) {
@@ -37,3 +42,31 @@ export function unwrapSuccess<T, E>(result: Result<T, E>): T {
 
   return result.val;
 }
+
+
+export function mapResult<T1, T2, E>(
+  result: Result<T1, E>,
+  fn: (val: T1) => T2
+): Result<T2, E> {
+  if (result.success) {
+    return Success(fn(result.val));
+  }
+
+  return Failure(result.err);
+}
+
+export function flatMapResult<T1, T2, E1, E2>(
+  result: Result<T1, E1>,
+  fn: (val: T1) => Result<T2, E2>
+): Result<T2, E1 | E2> {
+  if (result.success) {
+    return fn(result.val);
+  }
+
+  return Failure(result.err);
+}
+
+export {
+  andResult,
+  andResultFn
+};
