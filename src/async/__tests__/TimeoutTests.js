fd48eb2fde06b958f2812c0304afccdc8694b739
Vladimir Kurchatkin <vladimir.kurchatkin@gmail.com>
2016-08-18 18:35:54 +0300

    Fix eslint issues

--- src/async/__tests__/TimeoutTests.js
+++ src/async/__tests__/TimeoutTests.js
@@ -3,7 +3,6 @@
 import assert from 'assert';
 import timeout from '../timeout.js';
 import sleep from '../sleep.js';
-import { unwrap } from '../../index.js';
 import {
   catchPromise,
   convertToError
@@ -12,15 +11,15 @@ import {
 class TimeoutTests {
 
   async testTimeoutSuccess() {
-    const val = await timeout(async () => 3, 0)
+    const val = await timeout(async () => 3, 0);
     assert.equal(val, 3);
   }
 
   async testTimeoutFailure() {
     const err = await catchPromise(
       timeout(async () => {
-         await sleep(100);
-       }, 0)
+        await sleep(100);
+      }, 0)
     );
 
     assert.equal(convertToError(err).message, 'Timeout');
